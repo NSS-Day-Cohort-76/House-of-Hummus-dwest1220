e7bbedfe986b4b179f14642065fa80e27f744c64
David West <davidpwest1220@gmail.com>
2025-04-08 10:19:46 -0500

    Added Entree HTML generation module

--- scripts/Entrees.js
+++ scripts/Entrees.js
@@ -1,4 +1,20 @@
 export const Entrees = async () => {
     const response = await fetch("http://localhost:8088/entrees") 
-    const entree = response.json()
+    const entree = await response.json()
+
+    let html =  `
+    <section class="entree-option-list">
+        <h2>Entrees</h2> 
+`
+
+const entreeHTML = entree.map((entree)=>{
+    return `
+        <div class="entree-option-container">
+            <input type="radio" name="entree" value="${entree.id}" /> ${entree.name}    
+        </div>
+    `
+})
+html += entreeHTML.join("")
+html += "</section>"
+return html
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 import { FoodTruck } from "./FoodTruck.js"
+import { Entrees } from "./Entrees.js"
 import { Veggies } from "./Vegetables.js"
 
 const mainContainer = document.querySelector("#container")
 
 const renderAllHTML = async () => {
+    const entreeHTML = await Entrees()
     const vegetableHTML = await Veggies()
     // FoodTruck() ??
 
     const composedHTML = `
         <h1>House of Hummus</h1>
             <article class="menu-options">
+                ${entreeHTML}
                 ${vegetableHTML}
             </article>
     `
